179d181f8d9ae7657788100004a4936673b22f1b
Sahana Balram <sahana2008balram@gmail.com>
2018-12-02 10:21:54 -0800

    Included code to display front-end

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const mongoose = require('mongoose');
 app.use(bodyParser.json());
 app.use(cors());
 
+// allows node to find static content
+app.use(express.static(__dirname + '/public'));
 // Creates the database entry
 app.get('/new/:urlToShorten(*)',(req, res, next)=>{
     var {urlToShorten} = req.params;
